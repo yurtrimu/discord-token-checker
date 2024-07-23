e549db6e7c2ceb2357bb790f9984590ff815feee
yurtrimu <86497412+yurtrimu@users.noreply.github.com>
2024-07-23 13:19:54 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,17 @@ int main()
 
     struct discord_user_data user_data;
     std::vector<struct discord_billing_data> billing_data;
+    std::vector<struct discord_connection_data> connection_data;
+    std::vector<struct discord_gift_data> gift_data;
 
-    std::string token = "{ TOKEN HERE }";
+    std::string discord_token = "{ PASTE YOUR TOKEN }";
 
-    DiscordChecker::Collect(user_data, billing_data, token);
+    DiscordChecker::Collect(user_data, billing_data, connection_data, gift_data, discord_token);
 
-    DiscordChecker::print_discord_user_data(user_data);
+    DiscordChecker::print_user_data(user_data);
     DiscordChecker::print_billing_data(billing_data);
+    DiscordChecker::print_gift_data(gift_data);
+    DiscordChecker::print_connection_data(connection_data);
 
     curl_global_cleanup();
 }
